ee8bd81b8657b60e36ff4d742fee482396d38d5a
dima1206 <32818228+dima1206@users.noreply.github.com>
2023-02-14 15:56:47 +0200

    Hard code private IPs for now

--- terraform/resources-ec2.tf
+++ terraform/resources-ec2.tf
@@ -1,5 +1,8 @@
 locals {
   amazon_linux_ami = "ami-06c39ed6b42908a36"
+
+  agent1_private_ip    = "10.1.0.10"
+  webserver_private_ip = "10.1.0.20"
 }
 
 resource "aws_key_pair" "my_ssh_pub_key" {
@@ -21,10 +24,11 @@ resource "aws_instance" "jenkins_controller" {
   })
 }
 
-resource "aws_instance" "jenkins_agent" {
+resource "aws_instance" "jenkins_agent1" {
   ami           = local.amazon_linux_ami
   instance_type = var.instance_type
   key_name      = "my_ssh_pub_key"
+  private_ip    = local.agent1_private_ip
 
   vpc_security_group_ids = [aws_security_group.jenkins_agent_sg.id]
   subnet_id              = module.jenkins_vpc.public_subnets[0]
