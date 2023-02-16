b18f6f16d65c46c00f4442debeb7a9eeb6160eca
dima1206 <32818228+dima1206@users.noreply.github.com>
2023-02-16 23:18:58 +0200

    Run the playbook whenever an instance changes

--- terraform/run-ansible.tf
+++ terraform/run-ansible.tf
@@ -3,6 +3,11 @@ resource "null_resource" "ansible_playbook" {
     working_dir = "../ansible"
     command = "ansible-playbook -i aws_ec2.yml --private-key ${var.my_ssh_private_key_path} -u ec2-user --vault-password-file ${var.ansible_vault_password_path} controller-playbook.yml"
   }
+
   # controller interracts with agents during the play
-  depends_on = [aws_instance.jenkins_controller, aws_instance.jenkins_agent1, aws_instance.webserver]
+  triggers = {
+    controller_arn = aws_instance.jenkins_controller.arn
+    agent1_arn = aws_instance.jenkins_agent1.arn
+    webserver_arn = aws_instance.webserver.arn
+  }
 }
